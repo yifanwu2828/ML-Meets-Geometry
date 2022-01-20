3f8f4a27db60e84bad9589223be77850557a7f6d
yifanwu2828 <yiw084@ucsd.edu>
2022-01-19 22:51:02 -0800

    Update

--- cse291/HW1/geometry.py
+++ cse291/HW1/geometry.py
@@ -1,3 +1,5 @@
+import argparse
+
 from math import pi, cos, sin
 
 import open3d as o3d
@@ -11,7 +13,7 @@ from icecream import ic
 a, b, c = 1, 1, 0.5
 
 vis = o3d.visualization.Visualizer()
-vis.create_window(visible = False)
+vis.create_window(visible=False)
 all_gemos = None
 
 def draw_geometries(geoms):
@@ -28,10 +30,10 @@ def draw_geometries(geoms):
     plt.imshow(np.asarray(img)[::-1, ::-1])
     for g in geoms:
         vis.remove_geometry(g)
-    plt.show()    
+    # plt.show()
 
 
-def create_arrow_from_vector(origin, vector, color = [1, 0, 1]):
+def create_arrow_from_vector(origin, vector, color=[1, 0, 1]):
     """
     origin: origin of the arrow
     vector: direction of the arrow
@@ -40,7 +42,7 @@ def create_arrow_from_vector(origin, vector, color = [1, 0, 1]):
     v /= np.linalg.norm(v)
     z = np.array([0, 0, 1])
     angle = np.arccos(z @ v)
-    
+
     arrow = o3d.geometry.TriangleMesh.create_arrow(0.05, 0.1, 0.25, 0.2)
     arrow.paint_uniform_color(color)
     T = np.eye(4)
@@ -93,41 +95,13 @@ def collect_gemos():
             dtype=np.float64,
         )
     )
-    return ellipsoid, cf, arrow , curve
+    return ellipsoid, cf, arrow, curve
+
 
 # -----------------------------------------------------------------------------
 
-def f(u, v):
-    "map f : R2 -> R3 | (u,v) to (x,y,z)"
-    assert -pi < u < pi
-    assert 0 < v < pi
-    return np.array([
-        [a * cos(u) * sin(v)],
-        [b * sin(u) * sin(v)],
-        [c * cos(v)],
-    ])
 
-
-def D_fp(u,v):
-    return np.array(
-        [
-            [-a * sin(u) * sin(v), a * cos(u) * cos(v)],
-            [b * cos(u) * sin(v), b * sin(u) * cos(v)],
-            [0, -c * sin(v)],
-        ]
-    )
-   
-
-def surface_normal(u,v):
-    dfp = D_fp(u,v)
-    fu = dfp[:, 0]
-    fv = dfp[:, 1]
-    cross = np.cross(fu, fv)
-    norm = np.linalg.norm(cross)
-    return cross / norm
-
-
-def gamma(t):
+def gamma(t:float):
     """
     Parameterized Curves
     """
@@ -138,7 +112,51 @@ def gamma(t):
     return gamma_t
 
 
-def Q2(geoms):
+def f(u, v):
+    "map f : R2 -> R3 | (u,v) to (x,y,z)"
+    assert -pi < u < pi
+    assert 0 < v < pi
+    return np.array(
+        [
+            [a * cos(u) * sin(v)],
+            [b * sin(u) * sin(v)],
+            [c * cos(v)],
+        ]
+    )
+
+
+def get_D_fp(u, v):
+    return np.array(
+        [
+            [-a * sin(u) * sin(v), a * cos(u) * cos(v)],
+            [b * cos(u) * sin(v), b * sin(u) * cos(v)],
+            [0, -c * sin(v)],
+        ]
+    )
+
+
+def get_surface_normal(u, v):
+    D_fp = get_D_fp(u, v)
+    f_u = D_fp[:, 0]
+    f_v = D_fp[:, 1]
+
+    N = np.cross(f_u, f_v)
+    N /= np.linalg.norm(N)
+    return N
+
+
+def get_D_Np(u, v, norm):
+    
+    return np.array(
+        [
+            [b * c * sin(u) * sin(v) ** 2, -2 * b * c * cos(u) * sin(v) * cos(v)],
+            [-a * c * cos(u) * sin(v) ** 2, -2 * a * c * sin(u) * sin(v) * cos(v)],
+            [a * b * cos(u) * cos(v), a * b * sin(u) * sin(v)],
+        ]
+    )
+
+
+def Q2(geoms, use_viewer=False):
     """
     Let p = ( pi/4 , pi/6 ) and v = (1, 0). Draw the curve of ( f ◦ γ)(t) on the surface
     of the ellipsoid.
@@ -147,43 +165,87 @@ def Q2(geoms):
 
     time_steps = np.arange(0, 1, 0.01)
     gamma_t = np.array([gamma(t) for t in time_steps])
-                        
+
     curve2D = np.vstack([p, gamma_t])
 
     f_curve3D = np.asarray([f(u, v).ravel() for u, v in curve2D])
-    
+
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(f_curve3D)
     geoms_with_curve = geoms + [pcd]
-    draw_geometries(geoms_with_curve)
+    if use_viewer:
+        o3d.visualization.draw_geometries(geoms_with_curve)
+    else:
+        draw_geometries(geoms_with_curve)
+    return pcd
+
 
 def flatten2list(arr):
+
     return arr.flatten().tolist()
 
-def Q3(geoms):
-    p = [pi/4, pi/6]
+
+def Q3_c(geoms, use_viewer=False):
+    """"""
+    p = [pi / 4, pi / 6]
     v = np.array([1, 0]).reshape(2, 1)
-    Dfp = D_fp(*p)
 
-    Dfp_v = Dfp @ v
-    ic(Dfp_v)
+    Dfp_v = get_D_fp(p[0], p[1]) @ v
 
-    fp = f(*p).flatten()
-    Dfpv_arrow =  create_arrow_from_vector(flatten2list(fp), flatten2list(Dfp_v), color=[1, 0.706, 0])
-    geoms_with_arrow = geoms + [Dfpv_arrow]
-    draw_geometries(geoms_with_arrow)
-    
+    fp = f(p[0], p[1])
+    Dfpv_arrow = create_arrow_from_vector(
+        flatten2list(fp), flatten2list(Dfp_v), color=[1, 0.706, 0]
+    )
+    geoms_with_Dfp = geoms + [Dfpv_arrow]
+
+    if use_viewer:
+        o3d.visualization.draw_geometries(geoms_with_Dfp)
+    else:
+        draw_geometries(geoms_with_Dfp)
+    return Dfpv_arrow
+
+
+def Q3_d(geoms, use_viewer=False):
+    """
+    what is the normal vecotr of the tangent plant at p
+    N(u,v) = f_u X f_v / ||f_u X f_v||
+    """
+    u, v = [pi / 4, pi / 6]
+    N = get_surface_normal(u, v)
+    N_arrow = create_arrow_from_vector(
+        flatten2list(f(u, v)), flatten2list(N), color=[1, 0, 0]
+    )
+    geoms_with_surface_normal = geoms + [N_arrow]
+    if use_viewer:
+        o3d.visualization.draw_geometries(geoms_with_surface_normal)
+    else:
+        draw_geometries(geoms_with_surface_normal)
+    return N_arrow
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--use_viewer", "-v", action="store_true")
+    args = parser.parse_args()
 
-    ellipsoid, cf, arrow , curve = collect_gemos()
+    ellipsoid, cf, arrow, curve = collect_gemos()
     orignal_gemos = [ellipsoid, cf, arrow] + curve
-    # o3d.visualization.draw_geometries(all_gemos)
-    
-    
+    all_gemos = orignal_gemos
+
     # # (2)
-    Q2(orignal_gemos)
-   
-    # (3)
-    Q3(orignal_gemos)
+    # curve3D = Q2(orignal_gemos, args.use_viewer)
+    # all_gemos.append(curve3D)
+
+    # # (3)c
+    # tangent3D = Q3_c(orignal_gemos, args.use_viewer)
+    # all_gemos.append(tangent3D)
+
+    # # (3)d
+    # surfanceNorm3D = Q3_d(orignal_gemos, args.use_viewer)
+    # all_gemos.append(surfanceNorm3D)
+    # o3d.visualization.draw_geometries(all_gemos)
+
+
+
+
+    
